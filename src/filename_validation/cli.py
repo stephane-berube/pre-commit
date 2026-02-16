ede0617e50844016b4a8fd240b87eaf438950782
Stephane Berube <stephane.berube@ised-isde.gc.ca>
2026-02-16 08:29:39 -0500

    cfncli

--- src/filename_validation/cli.py
+++ src/filename_validation/cli.py
@@ -1,25 +1,68 @@
-import re
+import yaml
 from pathlib import Path
 import argparse
 from typing import Sequence
 
-SNAKE_CASE_REGEX = re.compile('^[a-z_]+$')
 
 def is_valid_filename(filename: str, min_len: int = 3) -> bool:
-    # extract the name so that `/my/repo/x.py` becomes `x`
     name = Path(filename).stem
-
-    print(filename)
-    print(Path(filename).parents[0])
 
     if too_short := len(name) < min_len:
         print(f'Name too short ({min_len=}): {filename}')
 
-    if not_snake_case := SNAKE_CASE_REGEX.search(name) is None:
-        print(f'Filename is not in snake_case: {filename}')
-
-    failure = too_short or not_snake_case
+    failure = too_short
     return not failure
+
+
+def parse_cfn_cli(filename):
+    resources = []
+
+    with Path(filename).open('r') as file:
+        cfncli = yaml.load_safe(file, Loader=yaml.Loader)
+
+        for stage in cfncli['Stages']:
+            for resource_name, resource in stage.items():
+                if resource_name == 'Config':
+                    continue
+
+                template = Path(resource['Template']).resolve()
+                params = []
+
+                for key, value in resource.get('Parameters', {}):
+                    params.append(f'{key}={value}')
+                
+                resources.append({
+                    'Template:': template,
+                    'Parameters': params
+                })
+
+    return resources
+
+
+def find_targets(filepaths: list) -> list:
+    print('filepaths:')
+    print(filepaths)
+
+    for filepath in filepaths:
+        pth = Path(filepath)
+        filename = pth.name    # ex: "cfn-cli.yaml"
+        type = pth.parents[-1] # top-folder (ex: "foundational" or "products")
+
+        if type == 'products' and filename == 'cfn-cli.yaml':
+            cfn_cli_info = parse_cfn_cli(filename)
+
+            print(cfn_cli_info)
+
+    # name = Path(filename).stem
+    # TODO: if the .yaml file is in /products
+        # TODO: if name is cfn-cli, get parameters, get underlying template
+        # TODO: if name is another yaml (presumably an underlying template)
+            # Add to cfn-cli target as long as it wasn't part of a cfn-cli
+            # We'll run cfn-cli without parameters on those ones.
+    # TODO: if the .yaml file is in /foundational
+        # TODO: cfn-lint with its equivalent .json
+
+    return []
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -38,10 +81,13 @@ def main(argv: Sequence[str] | None = None) -> int:
         help='Minimum length for a filename.',
     )
 
+    print('uhhh...')
     args = parser.parse_args(argv)
+    targets = find_targets(args.filenames)
 
     results = [
         not is_valid_filename(filename, args.min_len)
         for filename in args.filenames
     ]
+
     return int(any(results))
