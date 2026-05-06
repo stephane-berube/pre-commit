ead2f12cdeaf3366861e67b17b11ba8805debde7
Stephane Berube <stephane.berube@ised-isde.gc.ca>
2026-05-05 20:30:34 -0400

    Add custom checks, bundle cfn_tools.
    Checks:
    * Duplicate stack names
    * Missing mandatory parameters
    
    We're bundling cfn_tools because it's part of the deprecated cfn-flip
    package: https://github.com/awslabs/aws-cfn-template-flip

--- src/cfncli_lint/cli.py
+++ src/cfncli_lint/cli.py
@@ -1,7 +1,9 @@
 import argparse
+import collections
 import yaml
 
 from cfnlint.api import lint_file, ManualArgs
+from .cfn_tools import load_yaml
 from pathlib import Path
 from typing import Sequence
 
@@ -41,8 +43,10 @@ def parse_cfn_cli(filename):
                 resources.append({
                     'CfnCliPath': filename,
                     'Packaged': packaged,
-                    'Parameters': [parameters],
+                    'Parameters': parameters,
+                    'Region': resource['Region'],
                     'ResourceName': resource_name,
+                    'StackName': resource['StackName'],
                     'Template': str(template_path)
                 })
 
@@ -75,41 +79,106 @@ def find_targets(filepaths: list) -> list:
     return targets
 
 
-def run_cfn_lint(resources: list):
+def run_cfn_lint(resource: dict):
+    cfncli_path = resource['CfnCliPath']
+    resource_name = resource['ResourceName']
+    template_path = Path(resource['Template'])
+    params = resource['Parameters']
+    packaged = resource['Packaged']
+    ignored_rules = []
+
+    if packaged:
+        ignored_rules.append('W3002')
+
+    # TODO: look into the feasibility of using our config file
+    #       and then tweaking it to add rules, params, etc.
+    # config = ConfigFileArgs()
+    config = ManualArgs(
+        ignore_checks=ignored_rules,
+        parameters=[params],
+        regions=['ca-central-1']
+    )
+
+    errors = lint_file(template_path, config)
+
+    # TODO: stderr / logger
+    for error in errors:
+        print(f'{cfncli_path}:{resource_name} - [{error.rule.id}] {error.message}')
+
+    return len(errors) > 0
+
+
+# FIXME: Different regions are allowed to use the same stackname
+def has_duplicate_stack_names(stack_names: list):
+    duplicate_stack_names = [item for item, count in collections.Counter(stack_names).items() if count > 1]
+
+    has_dupes = len(duplicate_stack_names) > 0
+
+    # TODO: stderr / logger
+    if has_dupes:
+        print('Duplicate stack names:')
+
+        for dupe in duplicate_stack_names:
+            print('* ' + dupe)
+
+    return has_dupes
+
+
+def check_file(resources: list):
     results = []
+    stack_names = []
 
     for resource in resources:
-        cfncli_path = resource['CfnCliPath']
-        resource_name = resource['ResourceName']
-        template_path = Path(resource['Template'])
-        params = resource['Parameters']
-        packaged = resource['Packaged']
-        ignored_rules = []
+        stack_names.append(resource['StackName'])
 
-        if packaged:
-            ignored_rules.append('W3002')
+        # cfn-lint checks
+        results.append(run_cfn_lint(resource))
 
-        # TODO: look into the feasibility of using our config file
-        #       and then tweaking it to add rules, params, etc.
-        # config = ConfigFileArgs()
-        config = ManualArgs(
-            ignore_checks=ignored_rules,
-            parameters=params,
-            regions=['ca-central-1']
-        )
+        # Check for missing params
+        results.append(has_missing_params(resource))
 
-        errors = lint_file(template_path, config)
-
-        for error in errors:
-            print(f'{cfncli_path}:{resource_name} - [{error.rule.id}] {error.message}')
-
-        failure = False
-        if len(errors) > 0:
-            failure = True
-
-        results.append(failure)
+    # Check for duplicate stack names
+    results.append(has_duplicate_stack_names(stack_names))
 
     return results
+
+
+def has_missing_params(resource: dict):
+    template_parameters = get_template_parameters(resource['Template'])
+    resource_parameter_names = resource['Parameters'].keys()
+    resource_name = resource['ResourceName']
+
+    missing_cfncli_parameters = []
+
+    for name, value in template_parameters.items():
+        if value.get('Default',  None) is None and name not in resource_parameter_names:
+            missing_cfncli_parameters.append(name)
+
+    has_missing_params = len(missing_cfncli_parameters) > 0
+
+    if has_missing_params:
+        print(f'Missing parameters for {resource_name}:')
+
+        for missing_param in missing_cfncli_parameters:
+            print(f'* {missing_param}')
+
+    return has_missing_params
+
+
+def get_template_parameters(template_path: str) -> dict:
+    """Get parameters of the CFN template.
+
+    Args:
+        template_path (str): Path of the CFN template file.
+
+    Returns:
+        dict: CFN template parameters.
+    """
+    with Path(template_path).open('r') as file:
+        file_contents = file.read()
+        template = load_yaml(file_contents)
+
+        return template.get('Parameters', {})
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -128,8 +197,6 @@ def main(argv: Sequence[str] | None = None) -> int:
     results = []
 
     for target in targets:
-        results.extend(run_cfn_lint(target))
+        results = check_file(target)
 
     return int(any(results))
-
-main()
